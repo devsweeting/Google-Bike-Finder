ba44ad1da3f5285aff20885501b113ef1443e518
Devin Sweeting and Quinn Miller <devin@>
2019-01-16 11:47:55 -0800

    add UI functionality to add user distance

--- src/api.js
+++ src/api.js
@@ -2,10 +2,10 @@ export class Api {
   constructor() {
 
   }
-  call() {
+  call(distance) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = 'https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=10&stolenness=stolen&access_token=6abd63b5c4500507bf28eb9c6a6a1089a63192780929787bf23681966e3bb3f7';
+      let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=${distance}&stolenness=stolen&access_token=6abd63b5c4500507bf28eb9c6a6a1089a63192780929787bf23681966e3bb3f7`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -5,14 +5,17 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
-  let newApi = new Api();
-  let promise = newApi.call();
-  promise.then(function(response) {
-    let body = JSON.parse(response);
-    body.bikes.forEach(function(bike) {
-      $('#solution').append("<li> Bike " + bike.title + "</li>");
+  $(".distance").submit(function(event){
+    event.preventDefault();
+    let distance = $("#user-distance").val();
+    console.log(distance);
+    let newApi = new Api();
+    let promise = newApi.call(distance);
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      body.bikes.forEach(function(bike) {
+        $('#solution').append("<li> Bike " + bike.title + "</li>");
+      });
     });
   });
-
-
 });
