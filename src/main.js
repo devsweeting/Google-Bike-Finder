ac22cf89cc94cb4815ee7ddad5ae3dc9bd357458
Devin Sweeting and Quinn Miller <quinn@>
2019-01-16 14:56:37 -0800

    make mapbox work

--- src/main.js
+++ src/main.js
@@ -3,20 +3,28 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import mapboxgl from 'mapbox-gl';
 
-$(document).ready(function() {
-  $(".distance").submit(function(event){
-    event.preventDefault();
-    let distance = $("#user-distance").val();
-    console.log(distance);
-    let newApi = new Api();
-    let promise = newApi.call(distance);
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      body.bikes.forEach(function(bike) {
-        $('#solution').append("<li> Bike " + bike.title + "</li>");
-      });
-    });
-  $("#return").show()
-  });
+
+// import mapboxgl from 'mapbox-gl';
+$('#map-form').submit(function() {
+  $('#location').val()
+})
+mapboxgl.accessToken = 'pk.eyJ1IjoibWlsbGVycXVpbm42NiIsImEiOiJjanF6cXZzOWYwMmNvNDZsamt4d2MzdGt6In0.vWmStGk3pdqYpZX-SGutKA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/satellite-streets-v10',
+    center: [-74.50, 40],
+    zoom: 9,
 });
+
+// Add zoom and rotation controls to the map
+map.addControl(new mapboxgl.NavigationControl());
+
+
+map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+}));
